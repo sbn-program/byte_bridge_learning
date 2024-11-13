eb9805ffce8d6a402c9cbd9c424214cbf96970ea
sobhan <sobhan168nourani@gmail.com>
2024-11-13 14:05:36 +0330

    complete design a header section of main page

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,14 @@ module.exports = {
   content: ["./src/*.{html,js}"],
   theme: {
     extend: {},
+    fontFamily: {
+      'abster': ['abster'],
+      'dana': ['dana'],
+      'warzone': ['warzone'],
+      'radin': ['radin'],
+      'khodkar': ['khodkar'],
+      'battle-bingo': ['battle-bingo']
+    }
   },
   plugins: [],
 }
